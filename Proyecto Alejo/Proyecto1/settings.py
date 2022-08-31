e9a1bf8df14f7a185d97af49ef4b723a9546015a
AlejoEngel <112031720+AlejoEngel@users.noreply.github.com>
2022-08-31 16:20:55 -0300

    Update settings.py

--- Proyecto Alejo/Proyecto1/settings.py
+++ Proyecto Alejo/Proyecto1/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Proyecto1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['C:/Users/ALEJO/Documents/Python Curso/Proyecto Alejo/Plantillas'],
+        'DIRS': [BASE_DIRS / 'Plantillas'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
